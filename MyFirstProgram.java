979a152afa81f4edb13b3a4c0429978f96580005
robrown97 <yourGitHub@email.com>
2017-02-15 13:25:51 +0000

    week1 of Java work

---
+++ MyFirstProgram.java
@@ -0,0 +1,26 @@
+/*
+*MyFirstProgram.java
+*@Author R Brown
+*24 Jan 2017
+*/
+
+// public means open
+//class means applications
+//Creating an application called MyFirstProgram - Class Signature
+public class MyFirstProgram{    
+
+
+	//2. Main method
+	//Engine of the program                                                         
+	public static void main(String args[])
+	{
+	
+	//3. Print out a message
+	//ln means print to a line
+	//"things in here are printed onto the screen"
+	System.out.println("I love urdick");
+	System.out.println("I love having friday off");
+	
+	}
+
+}
